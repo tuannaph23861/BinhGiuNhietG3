d3808eb2107a355fc74e832cbe8454129fdb3dca
huytoan6523 <toannhph23989@fpt.edu.vn>
2023-11-21 23:09:43 +0700

    update Util

--- BinhGiuNhiet_G3/src/main/java/com/binhgiunhiet_g3/utils/HibernateUtil.java
+++ BinhGiuNhiet_G3/src/main/java/com/binhgiunhiet_g3/utils/HibernateUtil.java
@@ -1,8 +1,5 @@
 package com.binhgiunhiet_g3.utils;
 
-
-
-
 import com.binhgiunhiet_g3.entity.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -29,9 +26,18 @@ public class HibernateUtil {
         properties.put(Environment.SHOW_SQL, "true");
 
         conf.setProperties(properties);
-//      conf.addAnnotatedClass(KhachHang.class);
-      conf.addAnnotatedClass(BinhGiuNhiet.class);
-      conf.addAnnotatedClass(BinhGiuNhietChiTiet.class);
+        conf.addAnnotatedClass(BinhGiuNhiet.class);
+        conf.addAnnotatedClass(BinhGiuNhietChiTiet.class);
+        conf.addAnnotatedClass(ChatLieu.class);
+        conf.addAnnotatedClass(GioHang.class);
+        conf.addAnnotatedClass(GioHangChiTiet.class);
+        conf.addAnnotatedClass(HoaDon.class);
+        conf.addAnnotatedClass(HoaDonChiTiet.class);
+        conf.addAnnotatedClass(KhachHang.class);
+        conf.addAnnotatedClass(KhuyenMai.class);
+        conf.addAnnotatedClass(MauSac.class);
+        conf.addAnnotatedClass(NhanVien.class);
+        conf.addAnnotatedClass(ThuongHieu.class);
 
         ServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .applySettings(conf.getProperties()).build();
